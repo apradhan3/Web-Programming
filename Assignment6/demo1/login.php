61c4171254677a7361dcf4ab803bb77d6bd9a736
apradhan3 <55218789+apradhan3@users.noreply.github.com>
2020-12-13 20:41:21 -0500

    Assignment#6

---
+++ Assignment6/demo1/login.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost"; 
+$user = "root"; 
+$pass = "root"; 
+$db = "mysql"; 
+
+$r = mysql_connect($host, $user, $pass);
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Connection established\n"; 
+}
+
+echo mysql_get_server_info() . "\n"; 
+
+mysql_close();
+
+?>
---
+++ Assignment6/demo1/welcome.php
@@ -0,0 +1,17 @@
+<?php
+   include('session.php');
+?>
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome G-Money <?php echo $login_session; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
+
+C:\Users\pradh\OneDrive\Desktop\webprogramming\Assignment6\demo1\welcome.php
